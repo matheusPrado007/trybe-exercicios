c88a34566398a96a35550186df7ee2f9d80e8ed4
Matheus Prado <peu_prado01@hotmail.com>
2022-07-21 22:08:22 -0300

    2-Exercício, some todos os valores contidos no array e imprima o resultado

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,5 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let number of numbers){
+/*for(let number of numbers){
     console.log(number)
+}*/
+
+let soma = 0;
+for(index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index]
+console.log(soma);
 }
