9ac76d3077f49d46f890639a0a6ce43229b7d82d
Matheus Prado <peu_prado01@hotmail.com>
2022-07-21 22:15:57 -0300

    3-Exercício, calcule e imprima a média aritmética dos valores contidos no array

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,11 +1,22 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
 
-/*for(let number of numbers){
+for(let number of numbers){
     console.log(number)
-}*/
+}
+
+//Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
 let soma = 0;
 for(index = 0; index < numbers.length; index += 1) {
     soma += numbers[index]
 console.log(soma);
+}
+
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let soma = 0;
+for(index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index]
+console.log(soma / numbers.length);
 }
