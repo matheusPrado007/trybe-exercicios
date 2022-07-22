fd8db2ccfe5420410adda2a6aa9fdb5c9f136f67
Matheus Prado <peu_prado01@hotmail.com>
2022-07-22 00:36:19 -0300

    Array do exercicio anterior dividido po 2

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -89,7 +89,12 @@ for (let index = 1; index < numbers.length; index += 1) {
 }
 console.log(menorNumero);*/
 
-
+let number = [];
 for (let index = 1; index <= 25; index += 1) {
-   console.log(index)
+    number.push(index)
+   console.log(number)
+    }
+
+    for (let index = 0; index < number.length; index += 1){
+        console.log(number[index] / 2)
     }
