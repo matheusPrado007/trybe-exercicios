3d5e9acd7895977da1aa6262687c2867183dd5b2
Matheus Prado <peu_prado01@hotmail.com>
2022-07-24 20:25:11 -0300

    Exercicio 3maior e menor palavra de um array

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,7 +1,7 @@
 //O fatorial é representado pelo sinal !
 //! = 4 x 3 x 2 x 1 = 24
-
-let numbers = [];
+//
+/*1let numbers = [];
 let multiplication = 1;
 
 for (let index = 1; index <= 10; index += 1){
@@ -11,3 +11,32 @@ for(let i = 1; i < numbers.length; i += 1){
     multiplication *= numbers[i];
 }
 console.log(multiplication)
+
+2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'tryber';
+
+let wordInverted = '';
+
+for (let index = 0 ; index < word.length; index += 1){
+   wordInverted += word[word.length - 1 - index];
+    }
+    console.log(wordInverted)*/
+    
+
+    let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+    let palavraMaior = array[0];
+    let palavraMenor = array[0];
+
+
+    for (let index = 0 ; index < array.length; index += 1){
+       if (array[index].length > palavraMaior.length)
+       palavraMaior = array[index]
+    }
+    for (let index = 0 ; index < array.length; index += 1){
+        if (array[index].length < palavraMenor.length)
+        palavraMenor = array[index]
+     }
+     console.log(palavraMaior);
+console.log(palavraMenor);
