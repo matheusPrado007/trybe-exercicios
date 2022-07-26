40f431a7ab5d22d0f7a0e7f62b2e0df2dc43db6d
Matheus Prado <peu_prado01@hotmail.com>
2022-07-26 01:12:47 -0300

    Exercicio 2 - Parte 2

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Funções.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Funções.js
@@ -1,5 +1,5 @@
 
-function verificaPalindrome(palindrome) {
+/*function verificaPalindrome(palindrome) {
     let reverse = palindrome.split('').reverse().join('');
     if (reverse === palindrome) {
       return true;
@@ -9,4 +9,17 @@ function verificaPalindrome(palindrome) {
   }
   
   console.log(verificaPalindrome('arara')); 
-  console.log(verificaPalindrome('desenvolvimento')); 
+  console.log(verificaPalindrome('desenvolvimento')); */
+
+let indiceMaior = 0;
+let valor = [2, 3, 6, 7, 10, 1];
+  function indiceM(){
+    for(let indice in valor){
+        if (valor[indiceMaior] < valor[indice]){
+           indiceMaior = indice;
+        }
+    }
+    return indiceMaior
+  }
+  
+  console.log(indiceM([2, 3, 6, 7, 10, 1])) 
