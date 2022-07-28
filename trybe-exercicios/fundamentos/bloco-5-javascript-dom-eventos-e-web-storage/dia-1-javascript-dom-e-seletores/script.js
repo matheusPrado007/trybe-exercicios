d3a2469f30f7e29d12631ab10305c1843286a2c6
Matheus Prado <peu_prado01@hotmail.com>
2022-07-27 21:56:04 -0300

    5º Exercicio concluido(criação do arquivo para o JavaScript)

---
+++ trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,21 @@
+
+     document.getElementById('texto').innerText = 'Com muitos conhecimento adiquiridos e trabalhando com programação';
+
+    document.getElementsByClassName("main-content")[0].style.backgroundColor = 'rgb(76,164,109)';
+
+    document.getElementsByClassName("center-content")[0].style.backgroundColor = 'white';
+    
+    document.getElementsByTagName('h1')[0].innerText = 'Exercício 5.1 - JavaScript';
+
+    /*document.getElementsByTagName("p")[0].style.textTransform = "uppercase";*/
+
+
+    let upperTxt = document.getElementsByTagName('p');
+
+    function upperCase (x){
+        return x.toUpperCase()
+    }
+    for (key of upperTxt){
+        key.innerText = upperCase(key.innerText);
+    }
+  
