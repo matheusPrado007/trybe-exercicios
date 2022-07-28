7a11c47694c73cb8da1c7738e7819cf22cacc607
Matheus Prado <peu_prado01@hotmail.com>
2022-07-27 22:22:43 -0300

    6º Exercicio concluido

--- trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,21 +1,23 @@
 
-     document.getElementById('texto').innerText = 'Com muitos conhecimento adiquiridos e trabalhando com programação';
+document.getElementById('texto').innerText = 'Com muitos conhecimento adiquiridos e trabalhando com programação';
 
-    document.getElementsByClassName("main-content")[0].style.backgroundColor = 'rgb(76,164,109)';
+document.getElementsByClassName("main-content")[0].style.backgroundColor = 'rgb(76,164,109)';
 
-    document.getElementsByClassName("center-content")[0].style.backgroundColor = 'white';
-    
-    document.getElementsByTagName('h1')[0].innerText = 'Exercício 5.1 - JavaScript';
+document.getElementsByClassName("center-content")[0].style.backgroundColor = 'white';
 
-    /*document.getElementsByTagName("p")[0].style.textTransform = "uppercase";*/
+document.getElementsByTagName('h1')[0].innerText = 'Exercício 5.1 - JavaScript';
+
+/*document.getElementsByTagName("p")[0].style.textTransform = "uppercase";*/
 
 
-    let upperTxt = document.getElementsByTagName('p');
+let upperTxt = document.getElementsByTagName('p');
 
-    function upperCase (x){
-        return x.toUpperCase()
-    }
-    for (key of upperTxt){
-        key.innerText = upperCase(key.innerText);
-    }
-  
+function upperCase(x) {
+    return x.toUpperCase()
+}
+for (key of upperTxt) {
+    key.innerText = upperCase(key.innerText);
+}
+console.log(document.getElementsByTagName('p')[0].innerText)
+console.log(document.getElementsByTagName('p')[1].innerText)
+console.log(document.getElementsByTagName('p')[2].innerText)
