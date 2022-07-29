62b1a5cb92fe7bcf459d8b9b887516e74754fa56
Matheus Prado <peu_prado01@hotmail.com>
2022-07-28 21:48:25 -0300

    2º Exercicio concluido(tag main filho do body)

--- trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -4,5 +4,8 @@ let txt = document.createTextNode("Exercício 5.2 - JavaScript DOM ");
 adcElemento.appendChild(txt);
 elementoPai.appendChild(adcElemento);
 
+let mainTg = document.createElement('main');
+elementoPai.appendChild(mainTg);
+mainTg.classList.add('main-content');
 
 
