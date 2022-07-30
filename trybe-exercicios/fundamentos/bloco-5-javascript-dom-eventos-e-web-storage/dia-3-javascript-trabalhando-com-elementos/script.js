c87392990b8c5853dfd394a03179cccf5dfbf8ed
Matheus Prado <peu_prado01@hotmail.com>
2022-07-30 01:01:41 -0300

    1º Exercicio concluido

--- trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-trabalhando-com-elementos/script.js
+++ trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-trabalhando-com-elementos/script.js
@@ -18,15 +18,28 @@ function createDaysOfTheWeek() {
 
   function createDays() {
     let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-    const daysList = document.querySelector('#days');
-  
+    const daysId = document.querySelector('#days');
+   
   
     for (let index = 0; index < decemberDaysList.length; index += 1) {
       const daysNumbers = decemberDaysList[index];
       const dayListN = document.createElement('li');
      dayListN.innerHTML = daysNumbers;
   
-      daysList.appendChild(dayListN);
+      daysId.appendChild(dayListN);
+     dayListN.classList.add('day');
+  
     };
   };
-  createDays()
+  createDays();
+
+  document.getElementsByClassName('day')[25].classList.add('holiday');
+  document.getElementsByClassName('day')[26].classList.add('holiday');
+  document.getElementsByClassName('day')[32].classList.add('holiday');
+  document.getElementsByClassName('day')[5].classList.add('friday');
+  document.getElementsByClassName('day')[12].classList.add('friday');
+  document.getElementsByClassName('day')[19].classList.add('friday');
+  document.getElementsByClassName('day')[26].classList.add('friday');
+  
+
+  
