a12ca3dde3e0da88ed726a618e4e172996738c35
Matheus Prado <peu_prado01@hotmail.com>
2022-08-15 02:36:16 -0300

    2-Exercicio concluido

--- trybe-exercicios/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ trybe-exercicios/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -10,8 +10,18 @@
     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
   }
 
-  testingScope(true);*/
+  testingScope(true);
 
   const testingScope = (false) ?  `Não devo ser utilizada fora do meu escopo (if)` : `Não devo ser utilizada fora meu escopo (else)`;
-  console.log(testingScope);
+  console.log(testingScope);*/
+
+  
+   
+  const sortOddsAndEvens = () => {
+   let = oddsAndEvens = [13, 3, 4, 10, 7, 2];
+   return oddsAndEvens.sort((a, b) => a - b);
+    
+  };
+ console.log(`Os números ${sortOddsAndEvens()} se encontram ordenados de forma crescente!`);
+ 
   
